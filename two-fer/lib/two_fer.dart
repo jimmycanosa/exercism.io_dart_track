917c956fe30edc52f23054a199ef66eef1da9730
Jimmy <jimmycanosajr@gmail.com>
2021-05-16 22:05:31 +0800

    Two-fer first solution in comment

--- two-fer/lib/two_fer.dart
+++ two-fer/lib/two_fer.dart
@@ -1,1 +1,9 @@
 String twoFer([String name = 'you']) => 'One for $name, one for me.';
+
+// String twoFer([String? name]) {
+//   var result = 'One for you, one for me.';
+//   if (name != null) {
+//     result = 'One for $name, one for me.';
+//   }
+//   return result;
+// }
