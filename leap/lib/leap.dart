990fab181362179efb561d9bc0ffcf36acbbb723
Jimmy <jimmycanosajr@gmail.com>
2021-05-22 21:01:41 +0800

    Leap Solution

--- leap/lib/leap.dart
+++ leap/lib/leap.dart
@@ -1,3 +1,15 @@
 class Leap {
-  // Put your code here
+  bool leapYear(int year) {
+    if ((year % 4) == 0) {
+      return true;
+    } else if ((year % 100) == 0) {
+      if ((year % 400) == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
 }
