bc94b714d0e0ef99f413191d56b753a9f6618f38
Jimmy <jimmycanosajr@gmail.com>
2021-05-24 21:22:47 +0800

    Scrabble score solution

--- scrabble-score/lib/scrabble_score.dart
+++ scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,39 @@
-// Put your code here
+int scabbleLetterValue(String letter) {
+  final Map<String, int> value = {
+    'A': 1,
+    'B': 3,
+    'C': 3,
+    'D': 2,
+    'E': 1,
+    'F': 4,
+    'G': 2,
+    'H': 4,
+    'I': 1,
+    'J': 8,
+    'K': 5,
+    'L': 1,
+    'M': 3,
+    'N': 1,
+    'O': 1,
+    'P': 3,
+    'Q': 10,
+    'R': 1,
+    'S': 1,
+    'T': 1,
+    'U': 1,
+    'V': 4,
+    'W': 4,
+    'X': 8,
+    'Y': 4,
+    'Z': 10,
+  };
+  return value[letter]!;
+}
+
+int score(String word) {
+  return word
+      .toUpperCase()
+      .split('')
+      .map((letter) => scabbleLetterValue(letter))
+      .fold(0, (total, value) => total + value);
+}
